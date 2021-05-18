fa01dd4ebf84e293cb3ffb10e03e7793f797803e
Sergey Borodulin <sergei1234565@yandex.ru>
2021-05-18 15:03:26 +0300

    BMW first commit

---
+++ scripst/scriptc.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const featureLinkElems = document.querySelectorAll('.feature__link');  //обЪявляется переменная, стрелочка
+    const featureSubElems = document.querySelectorAll('.feature-sub');  //переменная, список
+
+    featureLinkElems.forEach((btn, index) => {
+        btn.addEventListener('click', () => {
+            if (featureLinkElems[index].classList.contains('feature__link_active')) {
+                featureSubElems[index].classList.add('hidden');  //закрытие списка при нажатии на его стрелку
+                featureLinkElems[index].classList.remove('feature__link_active'); //стрелка как при закрытом списке
+            } else {
+            featureSubElems.forEach((featureSubElem) => {
+                featureSubElem.classList.add('hidden');  //Закрыть список при открытии нового
+            })
+            featureLinkElems.forEach((featureLinkElem) => {
+                featureLinkElem.classList.remove('feature__link_active');  //Изменяется положение стрелки (вниз)
+            })
+            featureSubElems[index].classList.remove('hidden'); //развернуть список
+            btn.classList.add('feature__link_active');
+        } //поменять положение стрелки (вверх)
+        })
+    })
+})
